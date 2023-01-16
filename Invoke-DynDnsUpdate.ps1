0690b5c71f8442ea4a1537d7d33e0243c2d9cab7
Schlee, Kevin <kevin.schlee@dataport.de>
2023-01-16 10:34:49 +0100

    Switched web request invokation method.

--- Invoke-DynDnsUpdate.ps1
+++ Invoke-DynDnsUpdate.ps1
@@ -29,14 +29,14 @@ PARAM (
     [ValidateSet('Strato', 'No-IP')]
     [string] $Provider
 )
-
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
 $STRATO_CFG = [PSCustomObject]@{
     Uri = 'https://dyndns.strato.com'
     UserAgent = 'SchleeKe - Invoke-DynDnsUpdate - 1.0.0.0 - https://github.com/schleeke/ubuntu-scripts'
 }
 $NOIP_CFG = [PSCustomObject]@{
     Uri = 'https://dynupdate.no-ip.com'
-    UserAgent = 'SchleeKe DynDNS/6.3-1.0.0.0 schleeke@github.com'
+    UserAgent = 'SchleeKe DynDNS/6.3-1.0.0.0 mail.schleeke@posteo.de'
 }
 
 function script:Get-ExternalIp() {
@@ -46,34 +46,42 @@ function script:Get-ExternalIp() {
         Write-Error -Message "Unable to get external IP address. HTTP answer $($webResult.StatusCode).";
         return;
     }
+    [string] $externalIpAddress = [string]::Empty;
     [string[]] $lines = $webResult.Content.Split([System.Environment]::NewLine);
     if ($lines.Count -lt 5) {
-        Write-Error -Message 'Unable to parse the HTML content.';
-        return;
+        [string] $tmp = $webResult.Content.Substring(0, 1000);
+        [int] $startIndex = $tmp.IndexOf('<title>');
+        $tmp = $tmp.Substring($startIndex + '<title>'.Length);
+        $startIndex = $tmp.IndexOf('</title>');
+        $tmp = $tmp.Substring(0, $startIndex).Trim();
+        $tmp = $tmp.Substring('My External IP address -'.Length).Trim();
+        $externalIpAddress = $tmp;
     }
-    [string] $externalIpAddress = [string]::Empty;
-    foreach ($line in $lines) {
-        [string] $line = $line.Trim();
-        if ([string]::IsNullOrEmpty($line)) { continue; }
-        if ($line.StartsWith('<title>My External IP address -')) {
-            $line = $line.Substring('<title>My External IP address -'.Length).Trim();
-            if ($line.EndsWith('</title>')) {
-                $line = $line.Substring(0, $line.Length - '</title>'.Length).Trim();
+    else {
+        foreach ($line in $lines) {
+            [string] $line = $line.Trim();
+            if ([string]::IsNullOrEmpty($line)) { continue; }
+            if ($line.StartsWith('<title>My External IP address -')) {
+                $line = $line.Substring('<title>My External IP address -'.Length).Trim();
+                if ($line.EndsWith('</title>')) {
+                    $line = $line.Substring(0, $line.Length - '</title>'.Length).Trim();
+                }
+                [bool] $validIp = $false;
+                try {
+                    [ipaddress]$ipaddr = [ipaddress]$line;
+                    $validIp = $true;
+                }
+                catch {
+                    $validIp = $false;
+                }
+                if ($validIp) {
+                    $externalIpAddress = $line;
+                    break;
+                }
             }
-            [bool] $validIp = $false;
-            try {
-                [ipaddress]$ipaddr = [ipaddress]$line;
-                $validIp = $true;
-            }
-            catch {
-                $validIp = $false;
-            }
-            if ($validIp) {
-                $externalIpAddress = $line;
-                break;
-            }
-        }
+        }    
     }
+
     if ([string]::IsNullOrEmpty($externalIpAddress)) {
         Write-Error 'Unable to determine external IP address.';
         return;
@@ -98,7 +106,6 @@ function script:Get-UpdateUri() {
     Write-Output $retVal;
 }
 
-
 $externalIpAddress = script:Get-ExternalIp;
 $apiUrl = script:Get-UpdateUri;
 $apiUrl = "$($apiUrl)/nic/update?hostname=$($DomainName)&myip=$($externalIpAddress)";
@@ -112,16 +119,29 @@ if ($PSCmdlet.ParameterSetName -eq 'ByConfig') {
         'Strato' { $usrAgent = $STRATO_CFG.UserAgent; }
         'No-IP' { $usrAgent = $NOIP_CFG.UserAgent; }
     }
-    $webResult = Invoke-WebRequest -Uri $apiUrl -Headers $headers -UseBasicParsing -UseDefaultCredentials -UserAgent $usrAgent;
+    Add-Type -AssemblyName System.Net.Http;
+    [System.Net.HttpWebRequest] $request = [System.Net.WebRequest]::Create($apiUrl);
+    $request.Headers.Add("Authorization", $encodedCreds);
+    $request.UserAgent = $usrAgent;
+    [System.Net.HttpWebResponse] $response = $request.GetResponse();
+    if ($response.StatusCode -ne [System.Net.HttpStatusCode]::OK) {
+        Write-Error -Message "DynDNS update failed. HTTP Code $($response.StatusCode).";
+        return;    
+    }
+    $str = $response.GetResponseStream();
+    $rd = [System.IO.StreamReader]::new($str);
+    $encodedCreds = $rd.ReadToEnd().Trim();
+    $rd.Close();
+    $str.Close();
 }
 else {
     $webResult = Invoke-WebRequest -Uri $apiUrl -Headers $headers -UseBasicParsing -UseDefaultCredentials;
+    if ($webResult.StatusCode -ne 200) {
+        Write-Error -Message "DynDNS update failed. HTTP Code $($webResult.StatusCode).";
+        return;
+    }
+    $encodedCreds = $webResult.Content.Trim();
 }
-if ($webResult.StatusCode -ne 200) {
-    Write-Error -Message "DynDNS update failed. HTTP Code $($webResult.StatusCode).";
-    return;
-}
-$encodedCreds = $webResult.Content.Trim();
 if ($encodedCreds.StartsWith('good') -or $encodedCreds.StartsWith('nochg')) {
     return;
 }
